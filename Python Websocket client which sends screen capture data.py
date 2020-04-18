713e8af4890ab1a77584cbc65f8e9298d889d186
Gene Ong <gene@dosec.com.au>
2020-04-18 10:43:35 +1000

    improved buggy operation of pop up display, and toggled settings of screen capture area. Still need to work on GUI and mouse selection of screen capture

--- Python Websocket client which sends screen capture data.py
+++ Python Websocket client which sends screen capture data.py
@@ -1,6 +1,4 @@
 #integrating WEbsockets and screen capture
-from pynput.mouse import Listener
-
 import time
 
 import cv2
@@ -9,8 +7,9 @@ import numpy
 import websocket
 
 with mss.mss() as sct:
+    
 
-    monitor = {"top": 40, "left": 0, "width": 800, "height": 640}
+    monitor = {"top": 40, "left": 0, "width": 1000, "height": 400}
 
     ws = websocket.WebSocket()
     ws.connect("ws://192.168.1.102/test")
@@ -25,6 +24,9 @@ with mss.mss() as sct:
         img = numpy.array(sct.grab(monitor))
         # print('Original Dimensions : ',img.shape)
 
+        # Display the picture
+        #cv2.imshow("Selected Screen", img)
+        
         #parameters of new image
         height = 8
         width = 10
@@ -57,6 +59,9 @@ with mss.mss() as sct:
 
         
         ws.send_binary(chunk)
+        # Press "q" to quit
+        if cv2.waitKey(25) & 0xFF == ord("q"):
+           cv2.destroyAllWindows()
         #ws.close()
     
         
